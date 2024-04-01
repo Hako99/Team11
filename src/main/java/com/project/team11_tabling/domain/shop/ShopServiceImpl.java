a4ec617eef29ddb6e6488f6d53d7743b7fc30186
Yejin <ggg06142@gmail.com>
2024-04-01 16:36:38 +0900

    fix: register shop() method

--- src/main/java/com/project/team11_tabling/domain/shop/ShopServiceImpl.java
+++ src/main/java/com/project/team11_tabling/domain/shop/ShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.project.team11_tabling.domain.shop;
 
 import com.project.team11_tabling.domain.shop.externalAPI.KakaoAPI;
 import com.project.team11_tabling.domain.shop.externalAPI.KakaoResponseDTO;
+import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
@@ -26,23 +27,22 @@ public class ShopServiceImpl implements ShopService{
   public ShopResponseDto registerShop(ShopRequestDto requestDto) {
     Shop shop = new Shop(requestDto);
     shop.setTime(randomTime());
+    shopRepository.save(shop);
     return new ShopResponseDto(shop);
   }
 
-  public Date[] randomTime(){
-    Calendar calendar1 = Calendar.getInstance();
-    Calendar calendar2 = Calendar.getInstance();
-    Date[] date = new Date[2];
+  public LocalTime[] randomTime(){
+    LocalTime[] date = new LocalTime[2];
     Random random = new Random();
 
     int openHour = random.nextInt(7,12);
     int closeHour = random.nextInt(17,23);
 
-    calendar1.set(Calendar.HOUR_OF_DAY, openHour);
-    calendar2.set(Calendar.HOUR_OF_DAY, closeHour);
+    LocalTime randomTime1 = LocalTime.of(openHour, 0);
+    LocalTime randomTime2 = LocalTime.of(closeHour, 0);
 
-    date[0] = calendar1.getTime();
-    date[1] = calendar2.getTime();
+    date[0] = randomTime1;
+    date[1] = randomTime2;
 
     return date;
   }
